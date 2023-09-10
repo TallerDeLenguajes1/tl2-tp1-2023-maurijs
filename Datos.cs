ba9d5b3477d864d8f49a9b84b3d6665d2a7a9ec9
Mauricio <maurijo.suarez@gmail.com>
2023-09-10 04:03:11 -0300

    creacion de clases derivadas de AccesoADatos y modificacion de la interfaz de usuario

--- Datos.cs
+++ Datos.cs
@@ -1,14 +1,29 @@
+using System.Text.Json;
 namespace EspacioPedidos
 {
-    public class AccesoADatos{
+    public abstract class AccesoADatos
+    {
         private string dataPath;
         public string DataPath { get => dataPath; set => dataPath = value; }
         public AccesoADatos(string DirArchivo)
         {
-           DataPath = DirArchivo;    
+            DataPath = DirArchivo;
         }
 
-        public  List<Cadete> cargarCadetes(){
+        /*public virtual List<Cadete> cargarCadetes()
+        {
+            // Si uso virtual debo escribir una definicion generica para este metodo, es decir, debo escribir alguna instruccion aqui
+            // si uso abstract no debo incluir nada aqui, pero la clase AccesoADatos se volvera abstracta
+        }*/
+        public abstract List<Cadete> cargarCadetes();
+    }
+    public class AccesoCSV : AccesoADatos
+    {
+        public AccesoCSV(string DirArchivo) : base(DirArchivo)
+        {
+        }     
+        public override List<Cadete> cargarCadetes()
+        {
             List<Cadete> cadetes = new List<Cadete>();
             var cadetesCargados = File.ReadAllLines(DataPath)
             .Skip(1).                           //Saltea el encabezado
@@ -18,5 +33,30 @@ namespace EspacioPedidos
             Console.WriteLine("Cadetes cargados correctamente");
             return cadetes;
         }
-    }   
+    }
+    public class AccesoJSON : AccesoADatos
+    {
+        public AccesoJSON(string DirArchivo) : base(DirArchivo)
+        {
+        }     
+        public override List<Cadete> cargarCadetes()
+        {
+
+            List<Cadete> cadetes = new List<Cadete>();
+            try
+            {
+                // Lee el contenido del archivo JSON
+                string jsonText = File.ReadAllText(DataPath);
+
+                // Deserializa el JSON en una lista de objetos Cadete
+                cadetes = JsonSerializer.Deserialize<List<Cadete>>(jsonText);
+                Console.WriteLine("Cadetes cargados correctamente");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cargar cadetes desde el archivo JSON: {ex.Message}");
+            }
+            return cadetes;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -5,44 +5,58 @@ public class Program
     public static void Main()
     {
         var random = new Random();
-        string DataPath = "cadetes.csv";
         //Cargo los datos de los cadetes
-        var Datos = new AccesoADatos(DataPath);
-        //Instancio una clase Informe que mostrara los datos y estadisticas 
-        var listadoCadetes = Datos.cargarCadetes();
-        int IdCadete;
-        string nombreCadeteria = "La Nona";
-        string telefono = "3858402343";
-        var cadeteria = new Cadeteria(nombreCadeteria, telefono, listadoCadetes);
-
-        for (int i = 0; i < 6; i++)
+        Console.WriteLine("1-Cargar desde archivo csv \n 2-Cargar desde archivo json");
+        int respuesta;
+        //Debo inicializarlo antes de los bloques if asi pueda trabajar fuera de ellos
+        AccesoADatos Datos = null;
+        if (int.TryParse(Console.ReadLine(), out respuesta))
         {
-            cadeteria.CrearPedido(i, (float)random.Next(2000,10000),"observacion", "nombre","telefono", "direccion", "referencia direccion");
-        }
-        for (int i = 0; i < 6; i++)
-        {
-            IdCadete = random.Next(listadoCadetes.Count);
-            cadeteria.AsignarCadeteAPedido(i, IdCadete);  
-            Console.WriteLine("Pedido "+ i + " asignado a "+ cadeteria.GetCadeteByID(IdCadete).Nombre);
-        }
-        //Reasigno el pedido 3 al cadete 2
-        cadeteria.ReasignarPedido(3,2);
-        Console.WriteLine("Pedido "+ 3 + " reasignado a "+ cadeteria.GetCadeteByID(2).Nombre);
-        cadeteria.PedidoEntregado(0);
-        Console.WriteLine("Pedido 0 entregado");
-        cadeteria.PedidoEntregado(1);
-        Console.WriteLine("Pedido 1 entregado");
-        cadeteria.PedidoEntregado(2);
-        Console.WriteLine("Pedido 2 entregado");
-        cadeteria.PedidoEntregado(3);
-        Console.WriteLine("Pedido 3 entregado");
-        cadeteria.PedidoEntregado(4);
-        Console.WriteLine("Pedido 4 entregado");
-        cadeteria.PedidoEntregado(5);
-        Console.WriteLine("Pedido 5 entregado");
-    
+            if (respuesta == 1)
+            {
+                Datos = new AccesoCSV("cadetes.csv");
+            } else if (respuesta == 2)
+            {
+                
+                Datos = new AccesoJSON("cadetes.json");
+            
+            }    
+            //Instancio una clase Informe que mostrara los datos y estadisticas 
+            var listadoCadetes = Datos.cargarCadetes();
+            string nombreCadeteria = "La Nona";
+            string telefono = "3858402343";
+            var cadeteria = new Cadeteria(nombreCadeteria, telefono, listadoCadetes);
+            int IdCadete;
 
-        var informe = new Informe(cadeteria);
-        informe.MostrarInfCompleto();
+            for (int i = 0; i < 6; i++)
+            {
+                cadeteria.CrearPedido(i, (float)random.Next(2000,10000),"observacion", "nombre","telefono", "direccion", "referencia direccion");
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                IdCadete = random.Next(listadoCadetes.Count);
+                cadeteria.AsignarCadeteAPedido(i, IdCadete);  
+                Console.WriteLine("Pedido "+ i + " asignado a "+ cadeteria.GetCadeteByID(IdCadete).Nombre);
+            }
+            //Reasigno el pedido 3 al cadete 2
+            cadeteria.ReasignarPedido(3,2);
+            Console.WriteLine("Pedido "+ 3 + " reasignado a "+ cadeteria.GetCadeteByID(2).Nombre);
+            cadeteria.PedidoEntregado(0);
+            Console.WriteLine("Pedido 0 entregado");
+            cadeteria.PedidoEntregado(1);
+            Console.WriteLine("Pedido 1 entregado");
+            cadeteria.PedidoEntregado(2);
+            Console.WriteLine("Pedido 2 entregado");
+            cadeteria.PedidoEntregado(3);
+            Console.WriteLine("Pedido 3 entregado");
+            cadeteria.PedidoEntregado(4);
+            Console.WriteLine("Pedido 4 entregado");
+            cadeteria.PedidoEntregado(5);
+            Console.WriteLine("Pedido 5 entregado");
+        
+
+            var informe = new Informe(cadeteria);
+            informe.MostrarInfCompleto();
+        } 
     }
 }
